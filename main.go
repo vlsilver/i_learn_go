cc88681f5f6e7a3ece1a87755287f416a499ee01
vlsilver <silvervlhorse128@gmail.com>
2021-11-04 21:00:30 +0700

    i learn types

--- main.go
+++ main.go
@@ -1,33 +1,27 @@
 package main
 
-import "fmt"
-
 func main() {
-	//- Khai báo biến
-	// var name type
-	//- Nếu không gán giá trị,
-	// go sẽ tự động hán khởi tạo giá trị zero tương ứng
-	// với kiểu dữ liệu của biến.
-	//- Không thể thay đổi kiểu dữ liệu của biến.
-	var name string // name sẽ được gán giá trị là ""
-	var age int // age sẽ được gán giá trị là 0
-	//- Khai báo biến có giá trị khởi tạo
-	// var name type = initialvalue
-	var age1 int = 29
-	//- Suy luận kiểu, Go có thể tự động suy ra kiểu của biến
-	// từ giá trị khởi tạo.
-	var age2 = 10
-	//- Khai báo nhanh
-	age6 := 20
-	//- Khai báo nhiều biến cùng một lúc
-	var name1,name2 string
-	var age3,age4 int = 30,50
-	// - Khai báo các biến có các kiểu khác nhau trong cùng một câu lệnh
-	var (
-		name3 = "name"
-		age5 = 29
-	)
-	name4,age7 := "naveen", 29
+	//- Kiểu bool true/false
+	checkA := true
+	//- Kiểu dữ liệu số (numberic type)
+	//+ int8, int16, int32, int64, int
+	intA := 1
+	//+ float32, float64 - không chính xác
+	floatA := 14.64 // Các giá trị dấy phẩy động hay số thực được go suy ra là kiểu float64
+	floatB := -3.3
+	floatResult := floatA / floatB
+	//+ complex64, complex128 - Số phức
+	complexA := 8 + 27i
+	complexB := 10 + 27i
+	complexResult := complexA + complexB
+	//+ byte - là một cách gọi khác của uint8 [0,255]
+	//+ rune - là một cách gọi khác của int32 [-2^31, 2^31-1]
+	//- Kiểu string
+	stringA := "vlsilver"
+	//- Ngôn ngữ Go rất nghiêm ngặt và chặt chẽ, nên chúng không cho phép
+	//tự đổng chuyển đổi(ép kiểu) kiểu dữ liệu.
+	sum := float64(intA) + floatA
+	println(checkA, intA, floatResult, complexA, complexResult, stringA, sum)
 
-	fmt.Println(name,name1,name2, name3, name4, age, age1, age2, age3, age4, age5, age6, age7)
+
 }
