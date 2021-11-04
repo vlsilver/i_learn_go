11b511ec6ac0282b4df9c37a7bb7684e44fec6d8
vlsilver <silvervlhorse128@gmail.com>
2021-11-04 22:50:03 +0700

    add more link

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# First GoLang
+# [First GoLang](https://techmaster.vn/posts/34967/series-golang-co-ban-phan-2-hello-world)
+# [Youtube](https://www.youtube.com/watch?v=RI9ngRqn9N4)
+# [Source Golang]()
--- main.go
+++ main.go
@@ -1,6 +1,14 @@
+//Mỗi file go đều bắt đầu với câu lệnh package name
+//Package được dùng để phân chia code cho các mục đích sử dịnh khác nhau
+// và tăng tính tái sử dụng, ở đây package name là name.
 package main
+
+//package fmt được import vào file.
+//những funtion của fmt library có thể được sử dụng trong file
+
 import "fmt"
 
+//Chương trình được thực thi bắt đầu từ một hàm main() duy nhất.
 func main() {
 	fmt.Println("Hello Go!")
 }
