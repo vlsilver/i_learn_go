827c896cddc6070d79c0fd7a900417da88a5fd06
vlsilver <silvervlhorse128@gmail.com>
2021-11-05 17:46:04 +0700

    add learn switch case

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package  main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main()  {
 	learnIf()
+	learnSwitch()
+	learnMltipleSwitch()
 }
 
 func learnIf()  {
@@ -12,5 +16,29 @@ func learnIf()  {
 		fmt.Printf("Num %v là số chẵn", num)
 	}else {
 		fmt.Printf("Num %v là số lẽ", num)
+	}
+}
+
+func  learnSwitch()  {
+	num := 10
+	switch num % 2 {
+	case 0:
+		fmt.Printf("Num %v là số chẵn\n",num)
+		break
+	case 1:
+		fmt.Printf("Num %v là số lẻ\n",num)
+		break
+	default:
+		break
+	}
+}
+
+func learnMltipleSwitch()  {
+	num := 10
+	switch num % 2 {
+	case 0,1:
+		fmt.Printf("Num %v là số",num)
+	default:
+		break
 	}
 }
