76c113df9cb2d2fb37f89166d0c645eb80c69762
vlsilver <silvervlhorse128@gmail.com>
2021-11-06 00:28:03 +0700

    learn pass by value

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-## [String](https://techmaster.vn/posts/34999/series-golang-co-ban-phan-14-kieu-du-lieu-string)
+## [Pass by value](https://www.youtube.com/watch?v=LBLN4Wu5U4w)
+### Value type not have pointer
+[](https://firebasestorage.googleapis.com/v0/b/idot7-dc7e1.appspot.com/o/go_value_type_pass.png?alt=media&token=b652c8ed-e16a-4447-8cb2-01f7aaac11b9)
+### Reference type have pointer
+[](https://firebasestorage.googleapis.com/v0/b/idot7-dc7e1.appspot.com/o/go_reference_type_pass.png?alt=media&token=0df3ad3d-ab5f-4412-962c-b31a5e508383)
--- main.go
+++ main.go
@@ -1,52 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"unicode/utf8"
-)
+import "fmt"
 
 func main() {
-	//Là một slice chưa các byte(int8)
-	//Để xác định chính xác từng phần tử trong chuỗi ta cần chuyển string về []rune
-	//Chuỗi là bất biến nên chúng ta không thể thay đổi chuỗi
-	//->Để thay đổi ta cần chuyển chuỗi về []rune, chúng ta thay đổi []slice
-	//->Và chuyển nó lại về dạng string
+	//Go makes "copies" of values when passed into functions
+	//Gruop A - value type: Strings, Ints, Floats, Booleans, Arrays, Structs
+	//Group B - reference type(pointer): Slices, Maps, Functions
+	name := "vlsilver"
+	updateName(name)
+	fmt.Println(name)
+	menu := map[string]int{"pie": 1, "coffe": 2}
+	updateMenu(menu)
+	fmt.Println(menu)
 
-	learnString := "vlsivỄr" // UTF-8
 
-	for i := 0; i < len(learnString); i++ {
-		fmt.Printf("%x -  %c\n", learnString[i], learnString[i])
-	}
+}
 
-	// Đoạn code ở trên xảy ra lỗi
-	//76 -  v
-	//6c -  l
-	//73 -  s
-	//69 -  i
-	//76 -  v
-	//e1 -  á
-	//bb -  »
-	//84 - ... // something here
-	//72 -  r
-	// Mã Unicode UTF-8 của Ễ là e1 & bb & 84
-	//-> khi in ra từng byte thì sẽ hiển thị không đúng.
-	//-> Chúng ta dùng đến rune (int32)
-	//->rune đại diện cho một điểm mã trong go, không quan trọng điểm mã chứa bao nhiêu byte.
-	learnStringRune := []rune(learnString)
-	fmt.Println(learnStringRune)
-	for i := 0; i < len(learnStringRune); i++ {
-		fmt.Printf("%x - %c\n", learnStringRune[i], learnStringRune[i])
-	}
-	//loop with string, cách dễ dàng để triển khai rune cho string
-	for index, rune := range learnStringRune {
-		fmt.Printf("%v - %c\n", index, rune)
-	}
-	//độ dài của String
-	fmt.Println(utf8.RuneCountInString(learnString))
-
-	// thay đổi giá trị của chuỗi
-	// learnString[0] = 01 -- error
-	learnStringRune[0] = []rune("a")[0]
-	fmt.Println(string(learnStringRune))
-
+func updateName(x string) {
+	x = "vl"
+}
+func  updateMenu(m map[string]int)  {
+	m["coffee"] = 4
 }
